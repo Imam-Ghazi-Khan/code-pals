e32d1cf4eabec1b46018e92a91300c8d055c9336
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-02-28 17:20:51 +0530

    made it a lil bit more responsive

--- src/components/CoderDetailsCard.jsx
+++ src/components/CoderDetailsCard.jsx
@@ -3,18 +3,15 @@ const CoderDetailsCard = () => {
     <div className="m-8 p-4 cursor-pointer rounded-2xl shadow-md bg-gray-900 md:w-2/5">
       <h1 className="p-2 font-bold">Details</h1>
       <div className="flex align-items justify-between">
-      <div className="m-2 p-4 w-1/3 rounded-lg cursor-pointer shadow-md bg-gray-800">
+      <div className="m-2 p-4 w-1/2 rounded-lg cursor-pointer shadow-md bg-gray-800">
         <p>Followers</p>
         <p className="font-bold text-2xl">0</p>
       </div>
-      <div className="m-2 p-4 w-1/3 rounded-lg cursor-pointer shadow-md bg-gray-800">
+      <div className="m-2 p-4 w-1/2 rounded-lg cursor-pointer shadow-md bg-gray-800">
         <p>Following</p>
         <p className="font-bold text-2xl">0</p>
       </div>
-      <div className="m-2 w-1/3 p-4 rounded-lg cursor-pointer shadow-md bg-gray-800">
-        <p>Ratings</p>
-        <p className="font-bold text-2xl">0</p>
-      </div>
+
       </div>
       <p className="p-2">Bio</p>
       <div className="m-2 p-4 rounded-lg cursor-pointer shadow-md bg-gray-800 flex justify-center">
