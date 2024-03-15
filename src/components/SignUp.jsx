3a98c2101f4ccd16df5a56b25b2393b37d0aa0b9
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-03-16 03:23:05 +0530

    made it a bit responsive

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -8,7 +8,7 @@ const SignUp = () => {
       <img src={CypherIMG} alt=""
       className='md:static fixed h-full md:w-1/2 w-full -z-10'/>
       <div className='flex justify-center items-center h-full md:w-1/2 w-full'>
-      <div className='p-4 bg-blue-950 rounded-lg bg-opacity-50'>
+      <div className='p-4 md:bg-transparent bg-blue-950 rounded-lg bg-opacity-50'>
         <div>
           <h1 className='mt-4 font-bold text-2xl'>Sign Up</h1>
           <p className='mt-4'>Enter your Email</p>
