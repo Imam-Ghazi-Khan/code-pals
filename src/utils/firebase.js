c34c117dfff3d8544351b433145de85b773f41c2
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-04-09 01:22:43 +0530

    hardcoded projectid

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "@firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "codepals-45aa9",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
@@ -19,3 +19,4 @@ const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
 export const storage = getStorage(app);
+
